369631c66b1c21d62a8f5ea829a6304525a332a8
Jay-at-193694594941 <jay.lingelbach@gmail.com>
2025-02-20 15:29:35 -0600

    added extra tiptap extensions and initial settings/styles

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
+## Description
+
+This is a google docs clone with real time editing, capable of concurrent multiple user edits.
+
 ## Getting Started
 
 First, run the development server:
--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,6 +1,15 @@
 'use client';
 
 import { useEditor, EditorContent } from '@tiptap/react';
+import Image from '@tiptap/extension-image';
+import ImageResize from 'tiptap-extension-resize-image';
+import Table from '@tiptap/extension-table';
+import TableCell from '@tiptap/extension-table-cell';
+import TableHeader from '@tiptap/extension-table-header';
+import TableRow from '@tiptap/extension-table-row';
+import TaskItem from '@tiptap/extension-task-item';
+import TaskList from '@tiptap/extension-task-list';
+
 import StarterKit from '@tiptap/starter-kit';
 
 export const Editor = () => {
@@ -12,7 +21,21 @@ export const Editor = () => {
           'focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text'
       }
     },
-    extensions: [StarterKit],
+    extensions: [
+      Image,
+      ImageResize,
+      StarterKit,
+      Table.configure({
+        resizable: true
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      TaskList,
+      TaskItem.configure({
+        nested: true
+      })
+    ],
     content: '<p>Hello World! 🌎️</p>'
   });
 
